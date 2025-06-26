cc6c31dbd1e77df1be4d702de5a5f6183391635f
creaper9487 <58578328+creaper9487@users.noreply.github.com>
2025-06-26 22:38:38 +0800

    fixes

--- furry-name-card/src/components/Introduction.vue
+++ furry-name-card/src/components/Introduction.vue
@@ -2,8 +2,8 @@
 
 </script>
 
-<template class="dark">
-  <div class="object-position: top; bg-white dark:bg-black">
+<template>
+  <div class="bg-white dark:bg-black">
     <div class="rounded-lg bg-slate-500 p-6">
       Yo
     </div>
--- furry-name-card/src/views/HomeView.vue
+++ furry-name-card/src/views/HomeView.vue
@@ -3,19 +3,24 @@ import Head from '@/components/head.vue'
 import Name from '@/components/name.vue'
 function triggerSurprise() {
   console.log('Surprise triggered! owo')
-}import Introduction from '@/components/Introduction.vue';
+}
+import Introduction from '@/components/Introduction.vue';
 </script>
 
 <template>
-  <div class="fixed inset-0 flex flex-col justify-center items-center">
-    <Head class="m-10 hover:active transform" @mouseover="triggerSurprise" />
-    <Name class=""/>
-  <div class="w-screen h-screen flex justify-center items-center">
-      <Introduction class="text-black"/>
+  <div class="w-screen h-screen flex flex-col justify-center items-center">
+    <Head class="m-10 flip-animation transform" @mouseover="triggerSurprise" />
+    <Name class="mb-10"/>
+    <Introduction class="text-black"/>
   </div>
 </template>
 <style scoped>
-.active{
+.flip-animation:hover {
+  animation: doFlip 0.6s ease-in-out;
+}
 
+@keyframes doFlip {
+  0% { transform: rotateY(0deg); }
+  100% { transform: rotateY(360deg); }
 }
 </style>
