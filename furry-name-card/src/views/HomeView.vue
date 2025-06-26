b7ea29461f461a10d71a62c8a928fa9fff14f700
creaper9487 <58578328+creaper9487@users.noreply.github.com>
2025-06-26 21:43:03 +0800

    meow

--- furry-name-card/src/views/HomeView.vue
+++ furry-name-card/src/views/HomeView.vue
@@ -1,9 +1,11 @@
 <script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
+
 </script>
 
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+  <div class="w-full h-full flex justify-center items-center">
+    <div class="rounded-lg bg-slate-500 p-6">
+      meow owo
+    </div>
+  </div>
 </template>
