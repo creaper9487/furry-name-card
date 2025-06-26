d72cb39cef18a4acacfb1cedaeaa0246d23f9190
creaper9487 <58578328+creaper9487@users.noreply.github.com>
2025-06-26 22:30:59 +0800

    head+name

---
+++ furry-name-card/src/components/name.vue
@@ -0,0 +1,8 @@
+<script>
+
+</script>
+<template>
+    <div class="rounded-lg bg-slate-500 p-6">
+        meow owo
+    </div>
+</template>
--- furry-name-card/src/views/HomeView.vue
+++ furry-name-card/src/views/HomeView.vue
@@ -1,11 +1,19 @@
 <script setup lang="ts">
-
+import Head from '@/components/head.vue'
+import Name from '@/components/name.vue'
+function triggerSurprise() {
+  console.log('Surprise triggered! owo')
+}
 </script>
 
 <template>
-  <div class="fixed inset-0 flex justify-center items-center">
-    <div class="rounded-lg bg-slate-500 p-6">
-      meow owo
-    </div>
+  <div class="fixed inset-0 flex flex-col justify-center items-center">
+    <Head class="m-10 hover:active transform" @mouseover="triggerSurprise" />
+    <Name class=""/>
   </div>
 </template>
+<style scoped>
+.active{
+
+}
+</style>
